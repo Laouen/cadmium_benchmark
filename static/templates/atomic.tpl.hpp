34b4933469cb51ffaf351fa034808ff19cd8013b
Laouen <laouen.belloli@gmail.com>
2018-05-03 19:11:09 -0300

    bugfix in the static atomic model template

--- static/templates/atomic.tpl.hpp
+++ static/templates/atomic.tpl.hpp
@@ -1,6 +1,6 @@
 /****************************** atomic model {model_number} *****************************************/
 template<typename TIME>
-class atomic_{model_number} : empty_atomic<ports, TIME> {{
+class atomic_{model_number} : public empty_atomic<ports, TIME> {{
 	public:
     	atomic_{model_number}() = default;
 }};
